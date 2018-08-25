5c60359c4543c0f5d2ee6c27b2cb1aab46fbd4ba
2745329043 <2745329043@qq.com>
2018-08-25 17:32:49 +0800

    完善bintray上传功能

--- app/src/main/java/com/ssf/commonkt/ui/main/MainActivity.kt
+++ app/src/main/java/com/ssf/commonkt/ui/main/MainActivity.kt
@@ -12,6 +12,10 @@ import com.ssf.framework.main.mvvm.activity.MVVMActivity
 @Route(path = IRouterConfig.AR_PATH_MAIN)
 class MainActivity :   MVVMActivity<ActivityMainBinding>(R.layout.activity_main) {
 
+    override fun initStatusBar() {
+
+    }
+
     override fun init() {
 
     }
