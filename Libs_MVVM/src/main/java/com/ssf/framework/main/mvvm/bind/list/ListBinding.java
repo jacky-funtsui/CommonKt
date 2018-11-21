1ae755d8fa0dbf3e6e5d1ce2e62ec6414e8172fd
hzz <Hzizhen3083@163.com>
2018-11-21 19:36:18 +0700

    使用观察ObservableList数据item变动更新适配器，适用以下示例场景：  fun setCheckBoxVisible(isVisible: Boolean) {         observableCollectList.forEachIndexed {index,it->             it.isVisible = isVisible             observableCollectList.set(index,it) //调用set重新绑定adapter#item的内容，而不用再操作适配器         }     }

--- Libs_MVVM/src/main/java/com/ssf/framework/main/mvvm/bind/list/ListBinding.java
+++ Libs_MVVM/src/main/java/com/ssf/framework/main/mvvm/bind/list/ListBinding.java
@@ -1,8 +1,11 @@
 package com.ssf.framework.main.mvvm.bind.list;
 
 import android.databinding.BindingAdapter;
+import android.databinding.ObservableArrayList;
+import android.databinding.ObservableList;
 import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.ssf.framework.main.mvvm.adapter.BaseBindingAdapter;
 import com.ssf.framework.refreshlayout.XRefreshLayout;
@@ -16,16 +19,18 @@ import java.util.List;
  * Created by Hzz on 2018/8/20
  */
 public class ListBinding {
+    private static final String TAG = "ListBinding";
+
     @BindingAdapter(value = "data")
     public static void bindRefreshLayoutData(XRefreshLayout refreshLayout, List data) {
         RecyclerView.Adapter adapter = refreshLayout.getMLoadMoreRecyclerView().getAdapter();
-        bindAdapterData(adapter, data);
+        bindAdapterData(refreshLayout.getMLoadMoreRecyclerView(), adapter, data);
     }
 
     @BindingAdapter(value = "data")
     public static void bindRecyclerViewData(RecyclerView recyclerView, List data) {
         RecyclerView.Adapter adapter = recyclerView.getAdapter();
-        bindAdapterData(adapter, data);
+        bindAdapterData(recyclerView, adapter, data);
     }
 
     /**
@@ -34,7 +39,36 @@ public class ListBinding {
      * @param adapter
      * @param data    刷新的数据
      */
-    private static void bindAdapterData(RecyclerView.Adapter adapter, List data) {
+    private static void bindAdapterData(RecyclerView view, RecyclerView.Adapter adapter, List data) {
+        if (adapter == null) {
+            Log.e(TAG, "未给RecyclerView绑定适配器,请检查");
+            return;
+        }
+
+        if (data instanceof ObservableArrayList && adapter instanceof BaseBindingAdapter) {
+            Object tag = view.getTag();
+            if (tag == null) {
+                ObservableListUpdateCallback callback = new ObservableListUpdateCallback((BaseBindingAdapter) adapter);
+                ((ObservableList) data).addOnListChangedCallback(callback);
+                if (adapter instanceof BaseBindingAdapter) {
+                    BaseBindingAdapter adt = (BaseBindingAdapter) adapter;
+                    ArrayList<Object> oldData = adt.getList();
+                    if (oldData.isEmpty()) {
+                        callback.onChanged((ObservableList) data);//初次绑定手动触发用来添加数据
+                    }
+                }
+                view.setTag(callback);
+            } else {
+                if (tag instanceof ObservableListUpdateCallback) {
+                    //不用处理，内部Callback会自动进行数据处理
+                } else {
+                    throw new RuntimeException("使用Observable的RecyclerView不能绑定其它类型的TAG");
+                }
+            }
+
+            return;
+        }
+
         if (adapter instanceof BaseBindingAdapter) {
             BaseBindingAdapter adt = (BaseBindingAdapter) adapter;
             ArrayList<Object> oldData = adt.getList();
@@ -54,10 +88,10 @@ public class ListBinding {
      * @param newDatas 新数据
      */
     private static void diffBindAdapterData(RecyclerView.Adapter adapter, List oldDatas, List newDatas) {
-        if(oldDatas.isEmpty()){
+        if (oldDatas.isEmpty()) {
             oldDatas.addAll(newDatas);
             adapter.notifyDataSetChanged();
-        }else {
+        } else {
             DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DiffCallBack(oldDatas, newDatas));
             oldDatas.clear();
             oldDatas.addAll(newDatas);
@@ -65,7 +99,7 @@ public class ListBinding {
         }
     }
 
-    private static void closeItemAnimatior(RecyclerView recyclerView,boolean isClose){
+    private static void closeItemAnimatior(RecyclerView recyclerView, boolean isClose) {
         recyclerView.getItemAnimator().setAddDuration(0);
         recyclerView.getItemAnimator().setMoveDuration(0);
         recyclerView.getItemAnimator().setRemoveDuration(0);
